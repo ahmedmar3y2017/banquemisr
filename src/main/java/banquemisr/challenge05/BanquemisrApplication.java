3f628f77cc02f9c0422f4d6baf617b938c6712a6
AhmedMarey <ahmed.marey@asset.com.eg>
2024-11-14 17:43:47 +0200

    -- init

---
+++ src/main/java/banquemisr/challenge05/BanquemisrApplication.java
@@ -0,0 +1,13 @@
+package banquemisr.challenge05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanquemisrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanquemisrApplication.class, args);
+	}
+
+}
